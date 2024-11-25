70db2d5da4d1f40a0c6ad3a3df64803853255eda
Roshan Chaudhari <61083597+theroshanchaudhari@users.noreply.github.com>
2024-11-24 21:57:48 -0500

    Initial Commit

---
+++ I am Rich 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich 2
+//
+//  Created by Roshan Chaudhari on 2024-11-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Rich 2Tests/I_am_Rich_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_2Tests.swift
+//  I am Rich 2Tests
+//
+//  Created by Roshan Chaudhari on 2024-11-24.
+//
+
+import Testing
+@testable import I_am_Rich_2
+
+struct I_am_Rich_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
